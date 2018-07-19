b3a6e9c3b7b7b5b9fcc2bf190e0640db0e6b8fa0
Scott Revell <srevell@whitepenny.com>
2018-07-19 11:04:48 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sanvio</title>
+    <link rel="stylesheet" href="/dist/style.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i" rel="stylesheet">
+</head>
+<body>
+
+    <div class="splash-container">
+
+            
+            <div class="splash-image">
+                <img src="/dist/img/mirakel.png" alt="">
+            </div>
+            <div class="splash-body">
+                <img class="splash-logo" src="/dist/img/logo.svg" alt="">
+                <h1>Stop scratching, itching, hiding  & hurting</h1>
+                <p>Mirakel will be launching online in 2019</p>
+                <p><a href="#">For a free sample click here</a></p>
+            </div>
+
+           
+
+    </div>
+    
+</body>
+</html>
