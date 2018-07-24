58ce09091670c996614684453794e870d8d99544
Scott Revell <srevell@whitepenny.com>
2018-07-24 14:08:10 -0400

    Changed Form Link

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 <h1>Stop Scratching, Itching, Hiding & Hurting</h1>
                 <p>Mirakel will be available over the counter (OTC) in 2019. Sign up now for product launch, special event, and special pricing information!</p>
 
-                <p>Available NOW by prescription only! Just send your request for your prescription to try a sample of Mirakel today! First 100 prescriptions will be filled and shipped for FREE! <a href="mailto:rwells@sanvio.com">CLICK HERE TO REQUEST</a></p>
+                <p>Available NOW by prescription only! Just send your request for your prescription to try a sample of Mirakel today! First 100 prescriptions will be filled and shipped for FREE! <a target="_blank" href="https://sanvio.wufoo.com/forms/rfcj9eh17uzlzk/">CLICK HERE TO REQUEST</a></p>
 
                 
                 
