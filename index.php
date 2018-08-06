a3a8e336aa8c52333233f7ef63ba118ff9b73353
Scott Revell <srevell@whitepenny.com>
2018-08-06 12:35:40 -0400

    Added GA Code

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <title>Sanvio</title>
     <link rel="stylesheet" href="/dist/style.css">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123010693-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-123010693-1');
+    </script>
 </head>
 <body>
 
